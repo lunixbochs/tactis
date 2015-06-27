4d29ad5cd3e283be826be01edd382ad360c57bff
Ryan Hileman <lunixbochs@gmail.com>
2015-06-27 11:51:54 -0700

    clean up header includes

--- src/cpu.c
+++ src/cpu.c
@@ -1,6 +1,4 @@
-#include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "cpu.h"
--- src/util.c
+++ src/util.c
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "util.h"
 
 char *strdup(const char *s) {
